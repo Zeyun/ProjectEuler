afa0d70d5306e8986f78056eebceca2b9fc8eaed
unknown <tongzeyun@gmail.com>
2026-03-22 13:18:04 -0500

    auto

--- javascript/common.js
+++ javascript/common.js
@@ -131,7 +131,16 @@ Object.assign(window.ATLib, {
             },
             body: JSON.stringify({subject: subject, payload: payload}) // Body data must match "Content-Type" header
         })
-
+    },
+    saveAsHTML: ()=>{
+        const html = document.documentElement.outerHTML;
+        const blob = new Blob([html], { type: "text/html" });
+        const url = URL.createObjectURL(blob);
+        const a = document.createElement("a");
+        a.href = url;
+        a.download = "page.html";
+        a.click();
+        URL.revokeObjectURL(url);
     }
 });
 
